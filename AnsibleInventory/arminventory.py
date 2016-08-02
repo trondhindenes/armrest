66aba9b47c77aa2d3c101742e904424202279019
Trond Hindenes <trond@hindenes.com>
2016-08-02 12:56:17 +0200

    Simple multi-instance support

--- AnsibleInventory/arminventory.py
+++ AnsibleInventory/arminventory.py
@@ -7,12 +7,19 @@ from ansible import utils
 
 operation = str(sys.argv[1])
 
+#if running multiple armrest instances, use this to separate them
+#for instance, if armrest_instance is set to "prod", the corresponding configuration section in ansible.cfg should be "armrest_prod"
+armrest_instance = ""
 
+if armrest_instance != "":
+	armrest_config = "armrest_" + armrest_instance
+else:
+	armrest_config = "armrest"
 
-armrest_uri = get_config(p, "armrest", "armrest_uri", "ARMREST_URI","")
+armrest_uri = get_config(p, armrest_config, "armrest_uri", "ARMREST_URI","")
 #print(armrest_uri)
-armrest_use_cache = get_config(p, "armrest", "armrest_use_cache", "ARMREST_USE_CACHE","")
-armrest_cache_lifetime_seconds = get_config(p, "armrest", "armrest_cache_lifetime_seconds", "ARMREST_CACHE_LIFETIME_SECONDS","")
+armrest_use_cache = get_config(p, armrest_config, "armrest_use_cache", "ARMREST_USE_CACHE","")
+armrest_cache_lifetime_seconds = get_config(p, armrest_config, "armrest_cache_lifetime_seconds", "ARMREST_CACHE_LIFETIME_SECONDS","")
 
 if (armrest_use_cache == True):
 	import requests_cache
